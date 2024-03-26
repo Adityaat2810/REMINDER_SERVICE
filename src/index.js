981eb526063387602ed65207f16700660776921b
aditya <adityasainics10@gmail.com>
2024-03-26 10:06:34 +0530

    set up basic cron jobs

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express =require('express')
 const bodyParser = require('body-parser')
 const {PORT} = require('./config/server-config')
 const {sendBasicEmail} = require('./services/email-service')
+const cron = require('node-cron')
 
 const setupAndStartServer= () => {
     const app = express();
@@ -10,12 +11,17 @@ const setupAndStartServer= () => {
 
     app.listen(PORT,()=>{
         console.log(`Server started at port ${PORT}`);
-        sendBasicEmail(
-            '"Support" <support@admin.com>',
-            'adityasainics10@gmail.com',
-            'this is a testing email',
-            'hey ,how are you I hope you like the support'
-        )
+        // sendBasicEmail(
+        //     '"Support" <support@admin.com>',
+        //     'adityasainics10@gmail.com',
+        //     'this is a testing email',
+        //     'hey ,how are you I hope you like the support'
+        // )
+
+        // every two minutes it runs
+        cron.schedule('*/2 * * * *', () => {
+            console.log('running a task every two minutes');
+        });
     });
 
 
